f104060618de499e127cd15f3e0f04d6a138c961
Dante Lendaro <dantelendaro@gmail.com>
2023-01-12 18:33:48 -0300

    Probando esto

---
+++ JS/Functions.js
@@ -0,0 +1,7 @@
+document.getElementById('boton').onclick=function () {
+        console.log("Chupame la pija");
+        document.getElementById('boton').innerHTML="CHUPALAAA";
+document.getElementById("demo").innerHTML="PERO BIEN CHUPADA!! iiiii";
+    
+}
+    
